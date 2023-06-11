d9695504338e09f9a566c41b7567946c87f5a086
scotty <scottadamr01@gmail.com>
2023-06-11 01:21:33 -0400

    add more affirmations

---
+++ commands/zen/focus.js
@@ -0,0 +1,10 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('focus')
+		.setDescription('Replies with focusing thoughts!'),
+	async execute(interaction) {
+		await interaction.reply('Restart, Reset and Refocus. Your time will come!');
+	},
+};
