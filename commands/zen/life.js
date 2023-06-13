ef194cc1af5a909e2146b3d286ec7963527ab6c4
scotty <scottadamr01@gmail.com>
2023-06-12 22:58:06 -0400

    add life command

---
+++ commands/zen/life.js
@@ -0,0 +1,10 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('life')
+		.setDescription('Replies with life experiences!'),
+	async execute(interaction) {
+		await interaction.reply('Life is not about the big things you do, but the small experiences you make!');
+	},
+};
